e92c0e103aa9f4fd97880e5910df2b7847bfe11a
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2020-02-20 07:48:26 -0300

    Stable Release

--- build.sh
+++ build.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-# LIBRARY PATH
-LIB_PATH='./lib/'
-
-# ORIG
-TMP_PATH="${LIB_PATH}tmp/"
-
 # Create module
 echo 'Building module...'
 tsc
-# echo 'Minifying...'
-# rm -f dist/module/*.min.js
-# for JS in dist/module/*.js; do
-#     MIN_JS=$(echo $JS | sed s/.js/.min.js/g)
-#     uglifyjs --compress --mangle --output $MIN_JS -- $JS
-#     rm -f $JS
-# done
+
+if  [ "$1" == 'minify' ]; then
+    echo 'Minifying...'
+    for JS in './dist/module/*.js'; do
+        uglifyjs --compress --mangle --output $JS -- $JS
+    done
+fi
 
 echo 'Build finish!'
